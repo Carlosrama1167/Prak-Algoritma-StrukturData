74ff1300369abf3c54762910effb1456e03b8baa
Carlos Fadellilah Rama <carlosrama1144@gmail.com>
2026-03-09 11:29:47 +0700

    tugasss

---
+++ Tugas/Minggu_3/Dosen.java
@@ -0,0 +1,16 @@
+package Tugas.Minggu_3;
+
+public class Dosen {
+    public String kode;
+    public String nama;
+    public Boolean jenisKelamin; // True = Pria, False = Wanita
+    public int usia;
+
+    public Dosen(String kode, String nama, Boolean jenisKelamin, int usia) {
+        this.kode = kode;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.usia = usia;
+    }
+}
+
