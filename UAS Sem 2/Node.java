e3bc3be98b6503e985425c596dad7c4422faf15b
Carlos Fadellilah Rama <carlosrama1144@gmail.com>
2026-06-22 07:40:17 +0700

    uas sem 2

---
+++ UAS Sem 2/Node.java
@@ -0,0 +1,16 @@
+package UAS Sem 2;
+
+public class Node {
+    // Simpan bagian ini dalam file bernama Node.java (atau satukan di atas class utama)
+
+    String value;
+    Node left, right;
+
+    // Konstruktor untuk membuat node baru
+    public Node(String value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+}
